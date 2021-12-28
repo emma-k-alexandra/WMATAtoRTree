73253e87e4734ed69d70f0033bdb5431b215c245
Emma K Alexandra <emma@emma.sh>
2021-12-27 21:49:33 -0500

    Use new versions of rtree and wmata

--- Package.swift
+++ Package.swift
@@ -1,19 +1,33 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WMATAtoRTree",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/emma-k-alexandra/RTree.git", from: "2.1.0"),
-        .package(url: "https://github.com/emma-k-alexandra/WMATA.swift.git", from: "5.1.2")
+        .package(
+            name: "RTree",
+            url: "https://github.com/emma-k-alexandra/RTree.git",
+            .exact(.init(2, 2, 0))
+        ),
+        .package(
+            name: "WMATA",
+            url: "https://github.com/emma-k-alexandra/WMATA.swift.git",
+            from: "11.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "WMATAtoRTree",
             dependencies: [
                 "WMATA",
--- Sources/WMATAtoRTree/main.swift
+++ Sources/WMATAtoRTree/main.swift
@@ -7,20 +7,17 @@ var path = URL(fileURLWithPath: FileManager().currentDirectoryPath)
 path.appendPathComponent("Entrances.rtree")
 
 /// My R-Tree containing my spatial objects
-var tree = try RTree<Element>(path: path)
+var tree = try RTree<Element>(path: path, decoder: WMATAJSONDecoder())
 
-/// Get API key from environemnt
-guard let apiKey = ProcessInfo.processInfo.environment["WMATA_API_KEY"] else {
-    print("WMATA_API_KEY not set in environment")
-    exit(1)
-    
-}
+let TEST_API_KEY = "9e38c3eab34c4e6c990828002828f5ed" // Get your own @ https://developer.wmata.com using this one will probably result in some weird behavior
+
 
 /// Query Metro API for station entrances
-MetroRail(key: apiKey).entrances(at: nil) { result in
+Rail.StationEntrances(key: TEST_API_KEY).request { result in
     switch result {
     case .success(let entrances):
         for entrance in entrances.entrances {
+            print(entrance)
             /// create and insert elements into R-Tree
             let element = Element(point: Point2D(x: entrance.latitude, y: entrance.longitude), entrance: entrance)
             
@@ -33,7 +30,78 @@ MetroRail(key: apiKey).entrances(at: nil) { result in
         exit(1)
         
     }
-    
 }
 
 Thread.sleep(forTimeInterval: 60)
+
+/// Simple 2D vector
+struct Point2D: PointN {
+    typealias Scalar = Double
+    
+    var x: Scalar
+    var y: Scalar
+    
+    init(x: Scalar, y: Scalar) {
+        self.x = x
+        self.y = y
+    }
+    
+    func dimensions() -> Int {
+        2
+    }
+    
+    static func from(value: Double) -> Self {
+        Point2D(x: value, y: value)
+    }
+    
+    subscript(index: Int) -> Scalar {
+        get {
+            if index == 0 {
+                return self.x
+            } else {
+                return self.y
+            }
+            
+        }
+        set {
+            if index == 0 {
+                self.x = newValue
+            } else {
+                self.y = newValue
+            }
+        }
+    }
+}
+
+extension Point2D: Equatable {
+    static func == (lhs: Point2D, rhs: Point2D) -> Bool {
+        lhs.x == rhs.x && lhs.y == rhs.y
+    }
+}
+
+/// SpatialObject to store a 2d vector and a station entrance
+struct Element: SpatialObject {
+    typealias Point = Point2D
+        
+    let point: Point
+    let entrance: Rail.StationEntrances.Response.Entrance
+    
+    func minimumBoundingRectangle() -> BoundingRectangle<Point> {
+        return BoundingRectangle(lower: self.point, upper: self.point)
+    }
+    
+    func distanceSquared(point: Point) -> Double {
+        pow(point.x - self.point.x, 2) + pow(point.y - self.point.y, 2)
+    }
+    
+    func contains(point: Point) -> Bool {
+        self.distanceSquared(point: point) <= Point.Scalar.zero
+    }
+    
+}
+
+extension Element: Equatable {
+    static func == (lhs: Element, rhs: Element) -> Bool {
+        lhs.point == rhs.point
+    }
+}
